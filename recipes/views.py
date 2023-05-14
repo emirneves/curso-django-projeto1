4fef0991ce4ade147c0ed0c2593530d6433d1015
Emir Neves <emirneves@gmail.com>
2023-05-14 20:07:59 -0300

    Atualizacao

--- recipes/views.py
+++ recipes/views.py
@@ -1,19 +1,13 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html',context={
-       'name':'Emir Neves'
+    return render(request, 'recipes/home.html', context={
+        'name': 'Emir Neves',
     })
 
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-def sobre(request):
-    return HttpResponse('sobre')
 
 def my_ip(request):
-    #ip_publico =  Request.get('https://api.ipify.org/')
+    # ip_publico =  Request.get('https://api.ipify.org/')
     return HttpResponse('Mostrar Meu IP')
