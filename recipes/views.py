7e4ca5c397b91c6394e420b863445627a3105766
Emir Neves <emirneves@gmail.com>
2023-06-01 01:53:11 -0300

    Atualizando Aula 52

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
+
 from utils.recipes.factory import make_recipe
 
 
-# Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'recipes': [make_recipe() for _ in range(10)],
@@ -12,6 +12,7 @@ def home(request):
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': make_recipe(),
+        'is_detail_page': True,
     })
 
 
