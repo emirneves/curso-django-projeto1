e7518e1a6ddebe36f079e431cd77952ace62d400
Emir Neves <emirneves@gmail.com>
2023-06-05 00:26:58 -0300

    Erro na Aula 54 - admin.py

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,8 +1,11 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Category
 
-# from .models import Category
-
-# class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(admin.ModelAdmin):
 
 
-# admin.site.register(Category, CategoryAdmin)
+admin.site.register(Category, CategoryAdmin)    
+
+# PARADO AQUI EM 05/06/2023
+# REGISTREI O ERRO E FIZ A PERGUNTA NO GRUPO ( AULA 54) Minuto 9:25 / 13:56
+# https://www.udemy.com/course/curso-de-django-web-framework-com-python-html-e-css/learn/lecture/29515745#questions
