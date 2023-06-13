5e3be464982b3848fa4246022fdfacc4303ced13
Emir Neves <emirneves@gmail.com>
2023-06-13 10:35:37 -0300

    Atualização Aula 54 - Correção de Identação na Calasse CategoryAdmin

--- projeto/settings.py
+++ projeto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w)pn5!sb$y97((q#%u+$icz&k@4m1=eith(zgnhvj7r+1r9**2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # A gente tem apps também
+    # A gente tem apps tambï¿½m
     'recipes',
 ]
 
--- recipes/admin.py
+++ recipes/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
+
 from .models import Category
 
+
 class CategoryAdmin(admin.ModelAdmin):
+    ...
 
 
-admin.site.register(Category, CategoryAdmin)    
-
-# PARADO AQUI EM 05/06/2023
-# REGISTREI O ERRO E FIZ A PERGUNTA NO GRUPO ( AULA 54) Minuto 9:25 / 13:56
-# https://www.udemy.com/course/curso-de-django-web-framework-com-python-html-e-css/learn/lecture/29515745#questions
+admin.site.register(Category, CategoryAdmin)
